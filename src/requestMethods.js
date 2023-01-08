c268be9b62bb746a8e46ddcc8d1318143c684921
geludwig22 <jorlupeve@hotmail.com>
2023-01-08 08:43:10 -0300

    url

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api/";
+const BASE_URL = "https://finalproject.adaptable.app/";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
